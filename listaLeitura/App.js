1b0a99db6cf6ca6058b7669fc117db98b7d64209
Saulo Coelho <coelho.saulo@gmail.com>
2022-04-15 15:39:22 -0300

    start project

---
+++ listaLeitura/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View,Text, StyleSheet} from 'react-native'
+
+import Main from './src/pages/Main.js'
+
+const App = () => {
+  return(
+  <Main />
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
+export default App;
---
+++ listaLeitura/src/pages/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
+
+const Main = () => {
+  return(
+  <View>
+    <Text>Lista Leitura</Text>
+    <TouchableOpacity>
+        <Text>+</Text>
+    </TouchableOpacity>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
+export default Main;
